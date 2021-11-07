00d123ab77da9cea0857324ff8ab1080b6be849c
Fredrik Elias Freding <fredrikfreding04@gmail.com>
2021-11-07 22:12:55 +0100

    Stor forbedring.
    Har fjernet de individuelle HTML sidene, og heller få alt til å lastes inn på index. Dette gjør det mye enklere med tanke på local storage.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 // Variabler
 
 var utført = false;
+var sjanse = 0;
+var timeleft = 0;
+var rand_tall = rand_tall = Math.floor(Math.random() * 101) + 1;
+var penger = 0;
+var penge_nivå = 0;
+var reward = 0;
+var randnavn = "";
 
 // Lister
 const bil_privatperson = [
@@ -150,92 +157,83 @@ const penger_nivå = [
 function randomran() {
     var navn00f = Math.floor(Math.random() * random_fornavn.length);
     var navn00e = Math.floor(Math.random() * random_etternavn.length);
-    let navn01f = random_fornavn[navn00f];
-    let navn01e = random_etternavn[navn00e];
-
+    var navn01f = random_fornavn[navn00f];
+    var navn01e = random_etternavn[navn00e];
+    var penge_nivå = 0;
     var penga_nivå = Math.floor(Math.random() * 101) + 1;
+    
+    function randomIntFromInterval(min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
+      }
     
     if (penga_nivå > 0 && penga_nivå < 5) {
         penge_nivå = 5;
+        reward = randomIntFromInterval(3000, 5000)
       } else if (penga_nivå >= 5 && penga_nivå < 15) {
         penge_nivå = 4;
+        reward = randomIntFromInterval(2000, 3000)
       } else if (penga_nivå >= 15 && penga_nivå < 30) {
         penge_nivå = 3;
+        reward = randomIntFromInterval(1000, 2000)
       } else if (penga_nivå >= 30 && penga_nivå < 60) {
         penge_nivå = 2;
+        reward = randomIntFromInterval(500, 1000)
       } else if (penga_nivå >= 60 && penga_nivå < 70) {
         penge_nivå = 1;
+        reward = randomIntFromInterval(200, 500)
       } else if (penga_nivå >= 70 && penga_nivå < 101) {
         penge_nivå = 0;
+        reward = randomIntFromInterval(0, 200)
       }
 
-    var randnavn = navn01f + " " + navn01e;
+    randnavn = navn01f + " " + navn01e;
     sjanse = Math.floor(Math.random() * 101);
-    let peng_nivå = penger_nivå[penge_nivå]
+    peng_nivå = penger_nivå[penge_nivå]
     document.getElementById("ranoffer").innerHTML = "Offer:   " + randnavn;
     document.getElementById("sjanse").innerHTML = "Sjanse:   " + sjanse + "%";
     document.getElementById("penger_nivå").innerHTML = "Penger nivå:   " + peng_nivå;
-    document.getElementById("lagre").innerHTML = sjanse;
-    document.getElementById("lagre01").innerHTML = sjanse;
 }
-function våpen() {
-    if (document.getElementById("flexCheckDefault").checked) {
-        var våpen_sjansje = 0;
-        var p = document.getElementById('lagre');
-        våpen_sjansje = p.textContent;
-        våpen_sjansje = parseInt (våpen_sjansje, 10);
-        våpen_sjansje = våpen_sjansje + 30;
-        if (våpen_sjansje > 100) {
-            våpen_sjansje = 100;
-        }
-        document.getElementById("sjanse").innerHTML = "Sjanse:   " + våpen_sjansje + "%";
-        document.getElementById("lagre01").innerHTML = gammel_sjansje;
+
+function nytt() {
+    if (penger >= 200) {
+        penger = penger - 200;
+        document.getElementById("pengsjit").innerHTML = "<i class='fas fa-coins fa-fw'></i> Penger: " + penger;
+        randomran()
     } else {
-        var p = document.getElementById('lagre');
-        var gammel_sjansje = 0;
-        gammel_sjansje = p.textContent;
-        gammel_sjansje = parseInt (gammel_sjansje, 10);
-        document.getElementById("sjanse").innerHTML = "Sjanse:   " + gammel_sjansje + "%";
-        document.getElementById("lagre01").innerHTML = gammel_sjansje;
+        document.getElementById("mld").innerHTML = "Du har ikke nok penger!"
     }
 }
+function våpen() {
+        if (penger > 1500) {
+            var ekstra_sjansje = 30;
+            sjanse = sjanse + ekstra_sjansje
+            if (sjanse > 100) {
+                sjanse = 100;
+            }
+            document.getElementById("sjanse").innerHTML = "Sjanse:   " + sjanse + "%";
+            penger = penger - 1500;
+        }
+    }
 function utfør_ran() {
-    var rand_tall = 0;
-    var sjansje = 0;
-    rand_tall = Math.floor(Math.random() * 101) + 1;
 
-    var p = document.getElementById('lagre01');
-    sjansje = p.textContent;
-    sjansje = parseInt (sjansje, 10);
-
-    var reward = 0;
-
-    
     var myNode = document.getElementById("info01");
     while (myNode.firstChild) {
             myNode.removeChild(myNode.lastChild);
           }
     
-    document.getElementById("info01").innerHTML = "<div class='alignt-content-center'><p>Gratulerer, du klarte det!</p><p id='reward'></p></div><div id='countdown'></div>"
-    
-    if (sjansje > rand_tall) {
-
+    if (sjanse > rand_tall) {
+        document.getElementById("info01").innerHTML = "<div><p id='person' class='text-center'></p><br><div><p class='text-center' id='reward'></p></div><br></div>";
+        document.getElementById("reward").innerHTML = "Du tjente: " + reward + "kroner";
+        document.getElementById("person").innerHTML = "Gratuler! Du klarte å rane:<br><br>" + randnavn;
+        penger = penger + reward;
+        document.getElementById("pengsjit").innerHTML = "<i class='fas fa-coins fa-fw'></i> Penger: " + penger;
+    } else {
+        document.getElementById("info01").innerHTML = "<div><p id='person' class='text-center'></p><br><div><p class='text-center' id='reward'></p></div><br></div>";
+        document.getElementById("person").innerHTML = "Du klarte ikke å rane<br><be>" + randnavn;
     }
-    document.getElementById("lagre02").innerHTML = 70;
-    
-    utført = true
-    if (utført) {
-        console.log("Braaa")
-        cooldown()
-    }
+    timeleft = 70;
 }
 function cooldown(){
-    var timeleft = 0;
-    timeleft = document.getElementById('lagre02').textContent;
-    timeleft = parseInt (timeleft, 10);
-    localStorage.setItem("tid", timeleft);
-    timeleft = localStorage.getItem("tid");
-    
     if (timeleft > 0) {
         var myNode = document.getElementById("info01");
         while (myNode.firstChild) {
@@ -244,12 +242,48 @@ function cooldown(){
         document.getElementById("info01").innerHTML = "<div class='d-flex flex-column justify-content-center align-items-center w-100 h-100'><p id='p_01'>Du har fortsett:</p> <br> <p class='text-center fs-3' id='countdown'></p> <br> <p id='p_01'>sekunder igjen.</p></div>"
         var timer = setInterval(function(){
             if(timeleft <= 0){
-              clearInterval(timer);
-              location.reload();
+            clearInterval(timer);
+            endre("ran_privat")
+            randomran()
+            cooldown()
             } else {
               document.getElementById("countdown").innerHTML = timeleft;
             }
-            timeleft -= 1;
           }, 1000);
     }
+}
+
+setInterval(function(){ 
+	if (timeleft > 0) {
+        timeleft = timeleft - 1;
+    }
+}, 1000);
+
+
+var intervalId = window.setInterval(function(){
+    if (timeleft > 0) {
+        timeleft = timeleft - 1
+    } else {
+        clearInterval(intervalId) 
+    }
+  }, 1000);
+
+function endre(type) {
+    if (type === "ran_privat") {
+        var myNode = document.getElementById("info");
+        while (myNode.firstChild) {
+                myNode.removeChild(myNode.lastChild);
+              }
+        document.getElementById("info").innerHTML = "<div id='info01' class='border border-3 w-50 h-50 d-flex align-content-center shadow-lg p-3 mb-5 bg-body rounded flex-column'><p class='fs-1 text-center'>Ran en privatperson.</p><div class='align-self-center justify-content-center'><p class='fs-2 text-center'>Hvem vil du rane?</p><div class='align-self-center'> </div></div><button onclick='nytt()' id='finn_ran' type='button'class='btn btn-secondary w-50 align-self-center'>Finn ransoffer - Pris: 200kr</button><p id='mld' class='text-center'></p><p id='ranoffer' class='align-self-center'></p><p class='fs-5 align-self-center' id='sjanse'></p><p class='fs-5 align-self-center' id='penger_nivå'></p><button onclick='våpen()' type='button'class='btn btn-secondary w-50 align-self-center'>Kjøp og bruk et våpen - pris: 1 500</button><button onclick='utfør_ran()'id='utfør_ran' type='button' class='btn btn-secondary w-50 align-self-center'>Utfør ran</button></div>"
+    } else if (type === "main") {
+        var myNode = document.getElementById("info");
+        while (myNode.firstChild) {
+                myNode.removeChild(myNode.lastChild);
+              }
+        document.getElementById("info").innerHTML = "<div id='info01' class='border border-3 w-50 h-50 d-flex align-content-center shadow-lg p-3 mb-5 bg-body rounded flex-column'><div><h1 class='text-center'>Hovedkvarter</h1></div></div>"
+    }
+}
+
+function load() {
+    document.getElementById("pengsjit").innerHTML = "<i class='fas fa-coins fa-fw'></i> Penger: " + penger;
 }
