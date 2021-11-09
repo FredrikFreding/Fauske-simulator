71a7b2014fadbc3470b21400d555b5b50abdaa25
Fredrik Elias Freding <fredrikfreding04@gmail.com>
2021-11-09 02:08:37 +0100

    Store endringer

--- js/script.js
+++ js/script.js
@@ -9,10 +9,45 @@ var penge_nivå = 0;
 var reward = 0;
 var randnavn = "";
 var bil_sjanse = 0;
+var random_bil = 0;
+var bil_rand = 0;
+var bil_cd = 0;
+
+// Biler
+var Volvo240glturbo = 0;
+var Volvo740 = 0;
+var Volvo940 = 0;
+var Volvo240 = 0
+var ToyotaHiAce = 0;
+var BMWE30 = 0;
+var OpelKadett = 0;
+var OpelAstra = 0;
+var FordMondeo = 0;
+var VolkswagenUp = 0;
+var FerrariLaFerrari = 0;
+var Ferrari612Scaglietti = 0;
+var LamborghiniDiablo = 0;
+var ChevroletCorvetteZ06 = 0;
+
+var volvo240glturbo_pris = 20000;
+var Volvo740_pris = 23000;
+var Volvo940_pris = 25000;
+var Volvo240_pris = 17000
+var ToyotaHiAce_pris = 10000;
+var BMWE30_pris = 150000;
+var OpelKadett_pris = 30000;
+var OpelAstra_pris = 33000;
+var FordMondeo_pris = 40000;
+var VolkswagenUp_pris = 10000;
+var FerrariLaFerrari_pris = 400000;
+var Ferrari612Scaglietti_pris = 600000;
+var LamborghiniDiablo_pris = 900000;
+var ChevroletCorvetteZ06_pris = 400000;
 
 // Lister
-const bil_privatperson = [
-];
+const eide_biler = [
+    
+]
 const bil_offentlig_person = [
     "Ferrari LaFerrari",
     "Ferrari 612 Scaglietti",
@@ -29,8 +64,7 @@ const bil_gata = [
     "Opel Kadett",
     "Opel Astra",
     "Ford Mondeo",
-    "Volkswagen Up!",
-    "Toyota HiAce"
+    "Volkswagen Up!"
 ];
 const stjel_hus_gata = [
     "snus",
@@ -152,17 +186,133 @@ const penger_nivå = [
 
 
 // Funksjoner
+function utfør_bil() {
+    var bilpris = 0;
+    if (random_bil === "Volvo 240 GL Turbo") {
+       bilpris = volvo240glturbo_pris;
+    } else if (random_bil === "Volvo 740") {
+        bilpris = Volvo740_pris;
+    } else if (random_bil === "Volvo 940") {
+        bilpris = Volvo940_pris;
+    } else if (random_bil === "Volvo 240") {
+        bilpris = Volvo240_pris;
+    } else if (random_bil === "Toyota HiAce") {
+        bilpris = ToyotaHiAce_pris;
+    } else if (random_bil === "BMW E30") {
+        bilpris = BMWE30_pris;
+    } else if (random_bil === "Opel Kadett") {
+        bilpris = OpelKadett_pris;
+    } else if (random_bil === "Opel Astra") {
+        bilpris = OpelAstra_pris;
+    } else if (random_bil === "Ford Mondeo") {
+        bilpris = FordMondeo_pris;
+    } else if (random_bil === "Volkswagen Up!") {
+        bilpris = VolkswagenUp_pris;
+    }
+
+    var myNode = document.getElementById("info01");
+    while (myNode.firstChild) {
+            myNode.removeChild(myNode.lastChild);
+          }
+    document.getElementById("info01").innerHTML = "<p class='text-center' id='mld_bil'></p><p class='text-center' id='mld_bil00'></p>";
+          if (bil_sjanse > bil_rand) {
+            if (random_bil === "Volvo 240 GL Turbo") {
+                volvo240glturbo = volvo240glturbo + 1;
+             } else if (random_bil === "Volvo 740") {
+                 Volvo740 = Volvo740 + 1;
+             } else if (random_bil === "Volvo 940") {
+                 Volvo940 = Volvo940 + 1;
+             } else if (random_bil === "Volvo 240") {
+                 Volvo240 = Volvo240 + 1;
+             } else if (random_bil === "Toyota HiAce") {
+                 ToyotaHiAce = ToyotaHiAce + 1;
+             } else if (random_bil === "BMW E30") {
+                 BMWE30 = BMWE30 + 1;
+             } else if (random_bil === "Opel Kadett") {
+                 OpelKadett = OpelKadett + 1;
+             } else if (random_bil === "Opel Astra") {
+                 OpelAstra = OpelAstra + 1;
+             } else if (random_bil === "Ford Mondeo") {
+                 FordMondeo = FordMondeo + 1;
+             } else if (random_bil === "Volkswagen Up!") {
+                 VolkswagenUp = VolkswagenUp + 1;
+             }
+            document.getElementById("mld_bil").innerHTML = "Du klarte å stjele en: <br>" + random_bil;
+            document.getElementById("mld_bil00").innerHTML = "Bilen er verdt: <br>" + bilpris;
+          } else {
+            document.getElementById("mld_bil").innerHTML = "Du klarte ikke å stjele en: <br><br>" + random_bil;
+          }
+          bil_cd = 140;
+}
 
 function randombil() {
-    var random_bil = 0;
     var rand_bil_liste = Math.floor(Math.random() * bil_gata.length);
     random_bil = bil_gata[rand_bil_liste];
 
     bil_sjanse = Math.floor(Math.random() * 20) + 1;
+    bil_rand = Math.floor(Math.random() * 101);
 
-    document.getElementById("bil-type").innerHTML = "Du har funnet en " + random_bil + "!"
-    document.getElementById("bil_sjanse").innerHTML = bil_sjanse + "%"
+    document.getElementById("bil-type").innerHTML = "Du har funnet en " + random_bil + "!";
+    document.getElementById("bil_sjanse").innerHTML = bil_sjanse + "%";
 }
+
+function randombil0() {
+    var rand_bil_liste = Math.floor(Math.random() * bil_offentlig_person.length);
+    random_bil = bil_offentlig_person[rand_bil_liste];
+
+    bil_sjanse = Math.floor(Math.random() * 10) + 1;
+    bil_rand = Math.floor(Math.random() * 101);
+
+    document.getElementById("bil-type").innerHTML = "Du har funnet en " + random_bil + "!";
+    document.getElementById("bil_sjanse").innerHTML = bil_sjanse + "%";
+}
+function utfør_bil0() {
+    var bilpris = 0;
+    if (random_bil === "Ferrari LaFerrari") {
+       bilpris = FerrariLaFerrari_pris;
+    } else if (random_bil === "Ferrari 612 Scaglietti") {
+        bilpris = Ferrari612Scaglietti_pris;
+    } else if (random === "Lamborghini Diablo") {
+        bilpris = LamborghiniDiablo_pris;
+    } else if (random === "Chevrolet Corvette Z06") {
+        bilpris = ChevroletCorvetteZ06;
+    }
+
+    var myNode = document.getElementById("info01");
+    while (myNode.firstChild) {
+            myNode.removeChild(myNode.lastChild);
+          }
+    document.getElementById("info01").innerHTML = "<p class='text-center' id='mld_bil'></p><p class='text-center' id='mld_bil00'></p>";
+          if (bil_sjanse > bil_rand) {
+            if (random_bil === "Ferrari LaFerrari") {
+                FerrariLaFerrari = FerrariLaFerrari + 1;
+             } else if (random_bil === "Volvo 740") {
+                 Volvo740 = Volvo740 + 1;
+             } else if (random_bil === "Volvo 940") {
+                 Volvo940 = Volvo940 + 1;
+             } else if (random_bil === "Volvo 240") {
+                 Volvo240 = Volvo240 + 1;
+             } else if (random_bil === "Toyota HiAce") {
+                 ToyotaHiAce = ToyotaHiAce + 1;
+             } else if (random_bil === "BMW E30") {
+                 BMWE30 = BMWE30 + 1;
+             } else if (random_bil === "Opel Kadett") {
+                 OpelKadett = OpelKadett + 1;
+             } else if (random_bil === "Opel Astra") {
+                 OpelAstra = OpelAstra + 1;
+             } else if (random_bil === "Ford Mondeo") {
+                 FordMondeo = FordMondeo + 1;
+             } else if (random_bil === "Volkswagen Up!") {
+                 VolkswagenUp = VolkswagenUp + 1;
+             }
+            document.getElementById("mld_bil").innerHTML = "Du klarte å stjele en: <br>" + random_bil;
+            document.getElementById("mld_bil00").innerHTML = "Bilen er verdt: <br>" + bilpris;
+          } else {
+            document.getElementById("mld_bil").innerHTML = "Du klarte ikke å stjele en: <br><br>" + random_bil;
+          }
+          bil_cd = 140;
+}
+
 function randomran() {
     var navn00f = Math.floor(Math.random() * random_fornavn.length);
     var navn00e = Math.floor(Math.random() * random_etternavn.length);
@@ -262,9 +412,32 @@ function cooldown(){
     }
 }
 
+function cooldown_bil(){
+    if (bil_cd > 0) {
+        var myNode = document.getElementById("info01");
+        while (myNode.firstChild) {
+                myNode.removeChild(myNode.lastChild);
+              }
+        document.getElementById("info01").innerHTML = "<div class='d-flex flex-column justify-content-center align-items-center w-100 h-100'><p id='p_01'>Du har fortsett:</p> <br> <p class='text-center fs-3' id='countdown'></p> <br> <p id='p_01'>sekunder igjen.</p></div>"
+        var timer = setInterval(function(){
+            if(bil_cd <= 0){
+            clearInterval(timer);
+            endre("tilfeldig_bil")
+            randombil()
+            cooldown_bil()
+            } else {
+              document.getElementById("countdown").innerHTML = bil_cd;
+            }
+          }, 1000);
+    }
+}
+
 setInterval(function(){ 
 	if (timeleft > 0) {
         timeleft = timeleft - 1;
+    }
+    if (bil_cd > 0) {
+        bil_cd = bil_cd - 1;
     }
 }, 1000);
 
@@ -274,6 +447,11 @@ var intervalId = window.setInterval(function(){
         timeleft = timeleft - 1
     } else {
         clearInterval(intervalId) 
+    }
+    if (bil_cd > 0) {
+        bil_cd = bil_cd - 1;
+    } else {
+        clearInterval(intervalId)
     }
   }, 1000);
 
@@ -296,6 +474,24 @@ function endre(type) {
                 myNode.removeChild(myNode.lastChild);
               }
         document.getElementById("info").innerHTML = "<div id='info01' class='border border-3 w-50 h-50 d-flex align-content-center shadow-lg p-3 mb-5 bg-body rounded flex-column'><p class='fs-1 text-center'>Ran en privatperson.</p><div class='align-self-center justify-content-center'><p class='fs-2 text-center'>Hvem vil du rane?</p><div class='align-self-center'> </div></div><button onclick='nytt_offentlig()' id='finn_ran' type='button'class='btn btn-secondary w-50 align-self-center'>Finn ransoffer - Pris: 200kr</button><p id='mld' class='text-center'></p><p id='ranoffer' class='align-self-center'></p><p class='fs-5 align-self-center' id='sjanse'></p><button onclick='våpen()' type='button'class='btn btn-secondary w-50 align-self-center'>Kjøp og bruk et våpen - pris: 1 500</button><button onclick='utfør_ran()'id='utfør_ran' type='button' class='btn btn-secondary w-50 align-self-center'>Utfør ran</button></div>" 
+    } else if (type === "tilfeldig_bil") {
+        var myNode = document.getElementById("info");
+        while (myNode.firstChild) {
+                myNode.removeChild(myNode.lastChild);
+              }
+        document.getElementById("info").innerHTML = "<div id='info01' class='border border-3 w-50 h-50 d-flex align-content-center shadow-lg p-3 mb-5 bg-body rounded flex-column'><h1 class='text-center'>Stjel en tilfeldig bil</h1><div id='idkxd' class='w-100 align-self-center d-flex align-items-center justify-content-center flex-column'><p class='text-center fs-4 text' id='bil-type'></p><p class='text-center fs-4 text' id='bil_sjanse'></p></div><button onclick='nytt_bil()' id='finn_bil' type='button' class='btn btn-secondary w-50 align-self-center'>Finn en ny bil - 1 000kr</button><p id='finn_ny_bil' class='text-center'></p><button onclick='utfør_bil()' id='utført_bil' type='button' class='btn btn-secondary w-50 align-self-center align-text-bottom mt-auto'>Utfør ran</button></div>" 
+    } else if (type === "offentlig_bil") {
+        var myNode = document.getElementById("info");
+        while (myNode.firstChild) {
+                myNode.removeChild(myNode.lastChild);
+              }
+        document.getElementById("info").innerHTML = "<div id='info01' class='border border-3 w-50 h-50 d-flex align-content-center shadow-lg p-3 mb-5 bg-body rounded flex-column'><h1 class='text-center'>Stjel en bil fra en offentlig person</h1><div id='idkxd' class='w-100 align-self-center d-flex align-items-center justify-content-center flex-column'><p class='text-center fs-4 text' id='bil-type'></p><p class='text-center fs-4 text' id='bil_sjanse'></p></div><button onclick='nytt_bil0()' id='finn_bil' type='button' class='btn btn-secondary w-50 align-self-center'>Finn en ny bil - 10 000kr</button><p id='finn_ny_bil' class='text-center'></p><button onclick='utfør_bil0()()' id='utført_bil' type='button' class='btn btn-secondary w-50 align-self-center align-text-bottom mt-auto'>Utfør ran</button></div>"
+    } else if (type === "selg_biler") {
+        var myNode = document.getElementById("info");
+        while (myNode.firstChild) {
+                myNode.removeChild(myNode.lastChild);
+              }
+        document.getElementById("info").innerHTML = "<div id='info01' class='border border-3 w-50 h-50 d-flex align-content-center shadow-lg p-3 mb-5 bg-body rounded flex-column'><h1 class='text-center'>Dine biler</h1><ul class='list-group overflow-auto'><li class='list-group-item d-flex justify-content-between align-items-center'>Volvo 240 GL Turbo<span id='volvo240glturbo' class='badge bg-primary rounded-pill'></span></li><li class='list-group-item d-flex justify-content-between align-items-center'>Volvo 740<span id='volvo740' class='badge bg-primary rounded-pill'></span></li><span id='volvo740' class='badge bg-primary rounded-pill'></span> </li> <li class='list-group-item d-flex justify-content-between align-items-center'> Volvo 940 <span id='volvo940' class='badge bg-primary rounded-pill'></span> </li> <li class='list-group-item d-flex justify-content-between align-items-center'> Volvo 240 <span id='volvo240' class='badge bg-primary rounded-pill'></span> </li> <li class='list-group-item d-flex justify-content-between align-items-center'> Toyota HiAce <span id='toyotahiace' class='badge bg-primary rounded-pill'></span> </li> <li class='list-group-item d-flex justify-content-between align-items-center'> BMW E30 <span id='bmwe30' class='badge bg-primary rounded-pill'></span> </li> <li class='list-group-item d-flex justify-content-between align-items-center'> Opel Kadett <span id='opelkadett' class='badge bg-primary rounded-pill'></span> </li> <li class='list-group-item d-flex justify-content-between align-items-center'> Opel Astra <span id='opelastra' class='badge bg-primary rounded-pill'></span> </li> <li class='list-group-item d-flex justify-content-between align-items-center'> Ford Mondeo <span id='fordmondeo' class='badge bg-primary rounded-pill'></span> </li> <li class='list-group-item d-flex justify-content-between align-items-center'> Volkswagen Up! <span id='volkswagenup' class='badge bg-primary rounded-pill'></span> </li> <li class='list-group-item d-flex justify-content-between align-items-center'> Ferrari LaFerrari <span id='ferrarilaferrari' class='badge bg-danger rounded-pill'></span> </li> <li class='list-group-item d-flex justify-content-between align-items-center'> Ferrari 612 Scaglietti <span id='ferrari612scaglietti' class='badge bg-danger rounded-pill'></span> </li> <li class='list-group-item d-flex justify-content-between align-items-center'> Lamborghini Diablo <span id='lamborghinidablo' class='badge bg-danger rounded-pill'></span> </li> <li class='list-group-item d-flex justify-content-between align-items-center'> Chevrolet Corvette Z06 <span id='chevroletcorvettez06' class='badge bg-danger rounded-pill'></span> </li> </ul> <button class='btn btn-primary' type='button' onclick='endre('selg_biler')'>Selg biler</button> </div>"
     }
 }
 
@@ -314,7 +510,7 @@ function offentlig_ran() {
     reward = randomIntFromInterval(5000, 15000)
 
 
-    sjanse = 100;    //Math.floor(Math.random() * 20);
+    sjanse = Math.floor(Math.random() * 30 + 1);
     document.getElementById("ranoffer").innerHTML = "Offer:   " + randnavn;
     document.getElementById("sjanse").innerHTML = "Sjanse:   " + sjanse + "%";
 }
@@ -327,4 +523,41 @@ function nytt_offentlig() {
     } else {
         document.getElementById("mld").innerHTML = "Du har ikke nok penger!";
     }
+}
+
+function nytt_bil() {
+    if (penger >= 1000) {
+        penger = penger - 1000;
+        document.getElementById("pengsjit").innerHTML = "<i class='fas fa-coins fa-fw'></i> Penger: " + penger;
+        randombil()
+    } else {
+        document.getElementById("finn_ny_bil").innerHTML = "Du har ikke nok penger!"
+    }
+}
+
+function nytt_bil0() {
+    if (penger >= 10000) {
+        penger = penger - 10000;
+        document.getElementById("pengsjit").innerHTML = "<i class='fas fa-coins fa-fw'></i> Penger: " + penger;
+        randombil0()
+    } else {
+        document.getElementById("finn_ny_bil").innerHTML = "Du har ikke nok penger!"
+    }
+}
+
+function dinebiler() {
+    document.getElementById("volvo240glturbo").innerHTML = Volvo240glturbo;
+    document.getElementById("volvo740").innerHTML = Volvo740;
+    document.getElementById("volvo940").innerHTML = Volvo940;
+    document.getElementById("volvo240").innerHTML = Volvo240;
+    document.getElementById("toyotahiace").innerHTML = ToyotaHiAce;
+    document.getElementById("bmwe30").innerHTML = BMWE30;
+    document.getElementById("opelkadett").innerHTML = OpelKadett;
+    document.getElementById("opelastra").innerHTML = OpelAstra;
+    document.getElementById("fordmondeo").innerHTML = FordMondeo;
+    document.getElementById("volkswagenup").innerHTML = VolkswagenUp;
+    document.getElementById("ferrarilaferrari").innerHTML = FerrariLaFerrari;
+    document.getElementById("ferrari612scaglietti").innerHTML = Ferrari612Scaglietti;
+    document.getElementById("lamborghinidablo").innerHTML = LamborghiniDiablo;
+    document.getElementById("chevroletcorvettez06").innerHTML = ChevroletCorvetteZ06;
 }
